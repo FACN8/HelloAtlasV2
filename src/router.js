773d6e583f012f5b63aac80c4873a073fb026840
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-01-29 11:26:32 +0200

    Adding HelloAtlas Exising Project to New Version

---
+++ src/router.js
@@ -0,0 +1,20 @@
+const handlers = require('./handlers.js')
+
+/*--- Router Function Section ---*/
+const router = function(request, response) {
+    const url = request.url;
+    /*--- Managing Different URL ends ---*/
+    if (url === '/') {
+        handlers.handleHome(request, response);
+    } else if (url.indexOf('/public/') !== -1) {
+        handlers.handlePublic(request, response);
+    } else if (url.indexOf('/type') !== -1) {
+        handlers.handleType(request, response);
+    } else {
+        handlers.handleError(request, response);
+    }
+}
+
+/*--- Router Function Section - End ---*/
+
+module.exports = router;
