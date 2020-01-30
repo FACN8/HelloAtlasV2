073e2cce38f45d19db1135e405b4880f1c10a57b
LogicX1 <uninajone@gmail.com>
2020-01-30 15:07:01 +0200

    added more tests

--- src/routes.test.js
+++ src/routes.test.js
@@ -6,9 +6,57 @@ test('Home route should work', t=>{
     supertest(router)
     .get('/')
     .expect(200)
-    .expect('Content-Type','text/html')
+    .expect('Content-Type', /html/)
     .end ((err,res)=>{
             t.error(err)
             t.equal(res.type,'text/html','Response should return the index.html');
+            t.end();
     })
 })
+
+test('Public route should work for html files', t=>{
+    supertest(router)
+    .get('/public/index.html')
+    .expect(200)
+    .expect('Content-Type', /html/)
+    .end ((err,res)=>{
+            t.error(err)
+            t.equal(res.type,'text/html','Response should return the index.html');
+            t.end();
+    })
+})
+
+test('Public route should work for css files', t=>{
+    supertest(router)
+    .get('/public/style.css')
+    .expect(200)
+    .expect('Content-Type', 'text/css')
+    .end ((err,res)=>{
+            t.error(err)
+            t.equal(res.type,'text/css','Response should return the styles.css');
+            t.end();
+    })
+})
+test('Public route should work for javascript files', t=>{
+    supertest(router)
+    .get('/public/script.js')
+    .expect(200)
+    .expect('Content-Type', /javascript/)
+    .end ((err,res)=>{
+            t.error(err)
+            t.equal(res.type,'application/javascript','Response should return the styles.css');
+            t.end();
+    })
+})
+test('Public route should work for Images', t=>{
+    supertest(router)
+    .get('/public/res/BackGround.jpg')
+    .expect(200)
+    .expect('Content-Type', /image/)
+    .end ((err,res)=>{
+            t.error(err)
+            t.equal(res.type,'image/x-icon','Response should return the styles.css');
+            t.end();
+    })
+})
+
