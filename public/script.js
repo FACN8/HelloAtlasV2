29d7e1fe5658053236ebaa4ac7a5ef933322d91c
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-01-30 12:34:29 +0200

    Country data api moved to backend

--- public/script.js
+++ public/script.js
@@ -1,112 +1,125 @@
- // A listener maybe for typing a character ? 
- var textInput = document.querySelector('#ajax');
- var countryContainer = document.querySelector('.country-container');
- var datalist = document.getElementById("json-datalist");
- var SubmitBtn = document.getElementById('submit-btn');
- var xhr = new XMLHttpRequest();
+var textInput = document.querySelector("#ajax");
+var searchForm = document.querySelector(".search-form");
+var countryContainer = document.querySelector(".country-container");
+var datalist = document.getElementById("json-datalist");
+var SubmitBtn = document.getElementById("submit-btn");
 
- document.querySelector('.search-form').addEventListener('submit', function(event) {
-     event.preventDefault();
- })
+/*--- Prevent form default action ---*/
+searchForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
 
- textInput.oninput = function() {
-     xhr.onreadystatechange = function() {
-         if (xhr.readyState == 4 && xhr.status == 200) {
-             var data = JSON.parse(xhr.responseText);
-             createDataSet(data);
-         }
-     }
-     xhr.open('GET', '/type/' + textInput.value, true);
-     xhr.send();
- }
+/*--- Typing listener Section ---*/
+textInput.oninput = function() {
+  axios
+    .get("/type/" + textInput.value)
+    .then(function(response) {
+      createDataSet(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
+/*--- Typing listener Section - End ---*/
 
- // Create drop down list of suggestions based on data
- function createDataSet(data) {
-     var datalist = document.getElementById("json-datalist");
-     while (datalist.firstChild) {
-         datalist.removeChild(datalist.firstChild);
-     }
-     data.forEach(function(element) {
-         var opt = document.createElement("option");
-         opt.value = element;
-         datalist.appendChild(opt);
-     });
+/*--- Create dataSet and autocomplete form ---*/
+function createDataSet(data) {
+  var datalist = document.getElementById("json-datalist");
+  while (datalist.firstChild) {
+    datalist.removeChild(datalist.firstChild);
+  }
+  data.forEach(function(element) {
+    var opt = document.createElement("option");
+    opt.value = element;
+    datalist.appendChild(opt);
+  });
+}
+/*--- Create dataSet and autocomplete form - End ---*/
 
- }
+/*--- submit and serach button section ---*/
+SubmitBtn.addEventListener("click", function() {
+  while (datalist.firstChild) {
+    datalist.removeChild(datalist.firstChild);
+  }
 
- // A listener for submit event / click
- var xhr2 = new XMLHttpRequest();
+  axios
+    .get("/countriesFromBackEnd", {
+      params: {
+        country: textInput.value
+      }
+    })
+    .then(function(response) {
+      var data = response.data;
+      removeCountry();
+      addCountryDetails(data);
+      textInput.value = "";
+    })
+    .catch(function(error) {
+      console.log(error);
+      console.log("This is the error");
 
- SubmitBtn.addEventListener('click', function() {
-     while (datalist.firstChild) {
-         datalist.removeChild(datalist.firstChild);
-     }
-     xhr2.onreadystatechange = function() {
-         if (xhr2.readyState == 4 && xhr2.status == 200) {
-             var data = JSON.parse(xhr2.responseText);
-             removeCountry();
-             addCountryDetails(data);
-             textInput.value = '';
-         }
-     }
-     xhr2.open('GET', 'https://restcountries.eu/rest/v2/all');
-     xhr2.send()
- })
+      textInput.value = "";
+    });
+});
+/*--- submit and serach button section - End ---*/
 
- function addCountryDetails(data) {
+/*--- Country Details Section ---*/
+/*--- Manage API response and add details ---*/
+function addCountryDetails(data) {
+  var country = "";
+  var capitalCity = "";
+  var population = 0;
+  var flag = "";
+  var region = "";
+  for (var i in data) {
+    if (
+      data[i].name.toString().toLowerCase() == textInput.value.toLowerCase()
+    ) {
+      country = data[i].name;
+      capitalCity = data[i].capital;
+      population = data[i].population
+        .toString()
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      flag = data[i].flag;
+      region = data[i].region;
+    }
+  }
 
-     var country = '';
-     var capitalCity = '';
-     var population = 0;
-     var flag = '';
-     var region = '';
-     for (var i in data) {
-         if (data[i].name.toString().toLowerCase() == textInput.value.toLowerCase()) {
-             country = data[i].name;
-             capitalCity = data[i].capital;
-             population = data[i].population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-             flag = data[i].flag;
-             region = data[i].region;
-         }
-     }
+  if (country !== "") {
+    var countryDiv = document.createElement("div");
+    var countryName = document.createElement("h1");
+    var countryCapital = document.createElement("h3");
+    var countryRegion = document.createElement("h3");
+    var countryPop = document.createElement("h3");
+    var countryFlag = document.createElement("img");
 
-     if (country !== '') {
+    countryDiv.className = "country-div";
 
-         var countryDiv = document.createElement('div');
-         var countryName = document.createElement('h1');
-         var countryCapital = document.createElement('h3');
-         var countryRegion = document.createElement('h3');
-         var countryPop = document.createElement('h3');
-         var countryFlag = document.createElement('img');
+    countryName.textContent = country;
+    countryCapital.textContent = "Capital City: " + capitalCity;
+    countryPop.textContent = "Population: " + population;
+    countryFlag.src = flag;
+    countryRegion.textContent = "Region: " + region;
 
-         countryDiv.className = 'country-div';
+    countryDiv.appendChild(countryName);
+    countryDiv.appendChild(countryCapital);
+    countryDiv.appendChild(countryPop);
+    countryDiv.appendChild(countryRegion);
+    countryDiv.appendChild(countryFlag);
 
-         countryName.textContent = country;
-         countryCapital.textContent = 'Capital City: ' + capitalCity;
-         countryPop.textContent = 'Population: ' + population;
-         countryFlag.src = flag;
-         countryRegion.textContent = 'Region: ' + region;
+    countryContainer.appendChild(countryDiv);
+  } else {
+    var missingCountry = document.createElement("h1");
+    var textNode = document.createTextNode("No such country!");
+    missingCountry.appendChild(textNode);
+    countryContainer.appendChild(missingCountry);
+  }
+}
+/*--- Country Details Section - End ---*/
 
-         countryDiv.appendChild(countryName);
-         countryDiv.appendChild(countryCapital);
-         countryDiv.appendChild(countryPop);
-         countryDiv.appendChild(countryRegion);
-         countryDiv.appendChild(countryFlag);
-
-         countryContainer.appendChild(countryDiv);
-     } else {
-         var missingCountry = document.createElement('h1');
-         var textNode = document.createTextNode('No such country!');
-         missingCountry.appendChild(textNode);
-         countryContainer.appendChild(missingCountry);
-
-     }
- }
-
- function removeCountry() {
-     if (countryContainer.childNodes.length > 0) {
-         for (var i = 0; i < countryContainer.childNodes.length; i++) {
-             countryContainer.removeChild(countryContainer.childNodes[i]);
-         }
-     }
- }
+/*--- Clean last search ---*/
+function removeCountry() {
+  while (countryContainer.firstChild) {
+    countryContainer.removeChild(countryContainer.firstChild);
+  }
+}
--- src/handlers.js
+++ src/handlers.js
@@ -1,57 +1,75 @@
 const fs = require("fs");
 const path = require("path");
-const createOptions = require("./logic.js")
+const url = require("url");
+const qs = require("querystring");
+const createOptions = require("./logic.js");
+const countrydata = require("./application.js");
 
-
-/* Handles home directory and opens index.html*/
+/*--- Handles home directory and opens index.html ---*/
 const handleHome = (request, response) => {
-    const indexFilePath = path.join(__dirname, '..', 'public', 'index.html');
-    fs.readFile(indexFilePath, (err, file) => {
-        if (err) {
-            response.writeHead(500);
-            response.end('Our Server is Down!');
-        } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(file)
-        }
-    })
-}
+  const indexFilePath = path.join(__dirname, "..", "public", "index.html");
+  fs.readFile(indexFilePath, (err, file) => {
+    if (err) {
+      response.writeHead(500);
+      response.end("Our Server is Down!");
+    } else {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.end(file);
+    }
+  });
+};
 
-/* Handles requests of files from public directory*/
+/*--- Handles requests of files from public directory ---*/
 const handlePublic = (request, response) => {
-    const endpoint = request.url;
-    const extension = endpoint.split(".")[1];
-    const extensionType = {
-        html: "text/html",
-        css: "text/css",
-        js: "application/javascript",
-        ico: "image/x-icon",
-        jpg: "image/x-icon",
-        png: "image/x-icon"
-    };
-    const filePath = path.join(__dirname, "..", endpoint);
-    fs.readFile(filePath, (error, file) => {
-        if (error) {
-            handleError(request, response);
-        } else {
-            response.writeHead(200, { "Content-Type": extensionType[extension] });
-            response.end(file);
-        }
-    });
+  const endpoint = request.url;
+  const extension = endpoint.split(".")[1];
+  const extensionType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/javascript",
+    ico: "image/x-icon",
+    jpg: "image/x-icon",
+    jpeg: "image/x-icon",
+    png: "image/x-icon"
+  };
+  const filePath = path.join(__dirname, "..", endpoint);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      handleError(request, response);
+    } else {
+      response.writeHead(200, { "Content-Type": extensionType[extension] });
+      response.end(file);
+    }
+  });
 };
 
-/* Handles typing requests*/
+/*--- Handles typing requests ---*/
 const handleType = (request, response) => {
-    const endpoint = request.url.split("/")[2];
-    response.writeHead(200);
-    response.end(JSON.stringify(createOptions(endpoint)))
+  const endpoint = request.url.split("/")[2];
+  response.writeHead(200);
+  response.end(JSON.stringify(createOptions(endpoint)));
 };
 
+const handleSearchCountry = (request, response) => {
+  response.writeHead(200, { "content-Type": "application/json" });
+  countrydata(function(error, res) {
+    if (error) {
+      console.log("error 3");
+    } else {
+      response.end(JSON.stringify(res));
+    }
+  });
+};
 
 const handleError = (request, response) => {
-    response.writeHead(404);
-    response.end("Oh no! File not Found!");
-}
+  response.writeHead(404);
+  response.end("Oh no! File not Found!");
+};
 
-
-module.exports = { handleHome, handlePublic, handleType, handleError }
+module.exports = {
+  handleHome,
+  handlePublic,
+  handleType,
+  handleSearchCountry,
+  handleError
+};
